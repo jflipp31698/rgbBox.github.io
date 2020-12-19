5dd6f08b6e1615ab6a33daaeae0c5e8a5c37ca07
jflipp31698 <48108288+jflipp31698@users.noreply.github.com>
2020-12-19 16:28:02 -0600

    Added index.html, style.css and index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const redBox = document.getElementById("redBox");
+const greenBox = document.getElementById("greenBox");
+const blueBox = document.getElementById("blueBox");
+
+function press(){
+    redBox.style.backgroundColor = "white";
+    greenBox.style.borderRadius = "50%";
+    blueBox.style.width = "50vh"
+}
